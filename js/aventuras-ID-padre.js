c6fe0d3ccb5fb62800fe01c397012d885c76e96b
valenciavguides <55606798+valenciavguides@users.noreply.github.com>
2026-01-16 01:31:04 +0100

    Add files via upload

---
+++ js/aventuras-ID-padre.js
@@ -0,0 +1,210 @@
+/**
+ * aventuras-ID-padre.js
+ * Módulo centralizado para el array de elementosIDpadre del padre
+ * Estructura: DATOS_PADRE[aventura][idioma].elementosIDpadre = array de elementosIDpadre
+ */
+
+export const DATOS_PADRE = {
+  Aventura1: {
+    es: {
+      elementosIDpadre: [
+        { tipo: "inicio", padreid: "padre-P-0", parada_id: "P-0", audio_id: "audio-P-0", reto_id: "R-2" },
+        { tipo: "tramo", padreid: "padre-TR-1", parada_id: "TR-1", audio_id: "audio-TR-1", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-1", parada_id: "P-1", audio_id: "audio-P-1", reto_id: "R-3" },
+        { tipo: "tramo", padreid: "padre-TR-2", parada_id: "TR-2", audio_id: "audio-TR-2", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-2", parada_id: "P-2", audio_id: "audio-P-2", reto_id: "R-4" },
+        { tipo: "tramo", padreid: "padre-TR-3", parada_id: "TR-3", audio_id: "audio-TR-3", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-3", parada_id: "P-3", audio_id: "audio-P-3", reto_id: "R-5" },
+        { tipo: "tramo", padreid: "padre-TR-4", parada_id: "TR-4", audio_id: "audio-TR-4", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-4", parada_id: "P-4", audio_id: "audio-P-4", reto_id: "R-6" },
+        { tipo: "parada", padreid: "padre-P-5", parada_id: "P-5", audio_id: "audio-P-5", reto_id: "R-7" },
+        { tipo: "tramo", padreid: "padre-TR-5", parada_id: "TR-5", audio_id: "audio-TR-5", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-6", parada_id: "P-6", audio_id: "audio-P-6", reto_id: "PZ-8" },
+        { tipo: "parada", padreid: "padre-P-7", parada_id: "P-7", audio_id: "audio-P-7", reto_id: "R-9" },
+        { tipo: "parada", padreid: "padre-P-8", parada_id: "P-8", audio_id: "audio-P-8", reto_id: "R-10" },
+        { tipo: "parada", padreid: "padre-P-9", parada_id: "P-9", audio_id: "audio-P-9", reto_id: "R-11" },
+        { tipo: "parada", padreid: "padre-P-10", parada_id: "P-10", audio_id: "audio-P-10", reto_id: "R-12" },
+        { tipo: "tramo", padreid: "padre-TR-6", parada_id: "TR-6", audio_id: "audio-TR-6", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-11", parada_id: "P-11", audio_id: "audio-P-11", reto_id: "R-13" },
+        { tipo: "parada", padreid: "padre-P-12", parada_id: "P-12", audio_id: "audio-P-12", reto_id: "R-14" },
+        { tipo: "parada", padreid: "padre-P-13", parada_id: "P-13", audio_id: "audio-P-13", reto_id: "R-15" },
+        { tipo: "tramo", padreid: "padre-TR-7", parada_id: "TR-7", audio_id: "audio-TR-7", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-14", parada_id: "P-14", audio_id: "audio-P-14", reto_id: "R-16" },
+        { tipo: "parada", padreid: "padre-P-15", parada_id: "P-15", audio_id: "audio-P-15", reto_id: "R-17" },
+        { tipo: "tramo", padreid: "padre-TR-8", parada_id: "TR-8", audio_id: "audio-TR-8", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-16", parada_id: "P-16", audio_id: "audio-P-16", reto_id: "PZ-18" },
+        { tipo: "tramo", padreid: "padre-TR-9", parada_id: "TR-9", audio_id: "audio-TR-9", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-17", parada_id: "P-17", audio_id: "audio-P-17", reto_id: "R-19" },
+        { tipo: "parada", padreid: "padre-P-18", parada_id: "P-18", audio_id: "audio-P-18", reto_id: "R-20" },
+        { tipo: "tramo", padreid: "padre-TR-10", parada_id: "TR-10", audio_id: "audio-TR-10", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-19", parada_id: "P-19", audio_id: "audio-P-19", reto_id: "R-21" },
+        { tipo: "tramo", padreid: "padre-TR-11", parada_id: "TR-11", audio_id: "audio-TR-11", reto_id: null },
+        { tipo: "tramo", padreid: "padre-TR-12", parada_id: "TR-12", audio_id: "audio-TR-12", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-20", parada_id: "P-20", audio_id: "audio-P-20", reto_id: "R-22" },
+        { tipo: "parada", padreid: "padre-P-21", parada_id: "P-21", audio_id: "audio-P-21", reto_id: "R-23" },
+        { tipo: "tramo", padreid: "padre-TR-13", parada_id: "TR-13", audio_id: "audio-TR-13", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-22", parada_id: "P-22", audio_id: "audio-P-22", reto_id: "R-24" },
+        { tipo: "parada", padreid: "padre-P-23", parada_id: "P-23", audio_id: "audio-P-23", reto_id: "R-25" },
+        { tipo: "tramo", padreid: "padre-TR-14", parada_id: "TR-14", audio_id: "audio-TR-14", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-24", parada_id: "P-24", audio_id: "audio-P-24", reto_id: "PZ-26" },
+        { tipo: "tramo", padreid: "padre-TR-15", parada_id: "TR-15", audio_id: "audio-TR-15", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-25", parada_id: "P-25", audio_id: "audio-P-25", reto_id: "R-27" },
+        { tipo: "tramo", padreid: "padre-TR-16", parada_id: "TR-16", audio_id: "audio-TR-16", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-26", parada_id: "P-26", audio_id: "audio-P-26", reto_id: "R-28" },
+        { tipo: "tramo", padreid: "padre-TR-17", parada_id: "TR-17", audio_id: "audio-TR-17", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-27", parada_id: "P-27", audio_id: "audio-P-27", reto_id: "R-29" },
+        { tipo: "tramo", padreid: "padre-TR-18", parada_id: "TR-18", audio_id: "audio-TR-18", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-28", parada_id: "P-28", audio_id: "audio-P-28", reto_id: "R-30" },
+        { tipo: "tramo", padreid: "padre-TR-19", parada_id: "TR-19", audio_id: "audio-TR-19", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-29", parada_id: "P-29", audio_id: "audio-P-29", reto_id: "R-31" },
+        { tipo: "tramo", padreid: "padre-TR-20", parada_id: "TR-20", audio_id: "audio-TR-20", reto_id: null },
+        { tipo: "tramo", padreid: "padre-TR-21", parada_id: "TR-21", audio_id: "audio-TR-21", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-30", parada_id: "P-30", audio_id: "audio-P-30", reto_id: "R-32" },
+        { tipo: "tramo", padreid: "padre-TR-22", parada_id: "TR-22", audio_id: "audio-TR-22", reto_id: null },
+        { tipo: "parada", padreid: "padre-P-31", parada_id: "P-31", audio_id: "audio-P-31", reto_id: "R-33" }
+      ]
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  },
+  Aventura2: {
+    es: {
+      elementosIDpadre: []
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  },
+  Aventura3: {
+    es: {
+      elementosIDpadre: []
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  },
+  Aventura4: {
+    es: {
+      elementosIDpadre: []
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  },
+  Aventura5: {
+    es: {
+      elementosIDpadre: []
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  },
+  AventuraFallas: {
+    es: {
+      elementosIDpadre: []
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  },
+  Aventura34km: {
+    es: {
+      elementosIDpadre: []
+    },
+    en: {
+      elementosIDpadre: []
+    },
+    fr: {
+      elementosIDpadre: []
+    },
+    it: {
+      elementosIDpadre: []
+    },
+    nl: {
+      elementosIDpadre: []
+    },
+    ja: {
+      elementosIDpadre: []
+    }
+  }
+};
+
+// Para uso en entornos CommonJS y navegador
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { DATOS_PADRE };
+} else {
+    window.DATOS_PADRE = DATOS_PADRE;
+}
