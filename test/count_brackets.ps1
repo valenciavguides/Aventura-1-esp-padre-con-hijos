22086393e19daddc967c8cdab36061da9cb14419
valenciavguides <55606798+valenciavguides@users.noreply.github.com>
2026-02-03 01:49:16 +0100

    Delete test directory

--- test/count_brackets.ps1
+++
@@ -1,7 +0,0 @@
-$path = "codigo-padre.html"
-$txt = Get-Content -Raw -Path $path
-$pairs = @("(", ")", "{", "}", "[", "]")
-foreach ($c in $pairs) {
-    $count = ([regex]::Matches($txt, [regex]::Escape($c))).Count
-    Write-Output "$c => $count"
-}
